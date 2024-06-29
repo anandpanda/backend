a27b79b2a294d4f1f878d456ac13253a6945e37d
Anand Panda <anand612p@gmail.com>
2024-06-30 04:31:02 +0530

    database connection (mongo db)

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../contants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+
+    console.log("Database connection successful.\n");
+    console.log("Connection instance: ", connectionInstance.connection.host);
+  } catch (error) {
+    console.error("Database connection failed.", error);
+    throw error;
+  }
+};
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,31 @@
+// require("dotenv").config({ path: "./.env" });
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+
+import connectDB from "./db/index.js";
+connectDB()
+
+/* // IIFE
+import express from "express";
+import mongoose from "mongoose";
+const app = express();
+;(
+    async () => {
+        try {
+            await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+            app.on("error", (error) => {
+                console.error(error);
+                throw error;
+            })
+
+            app.listen(process.env.PORT, () => {
+                console.log(`Server is running on port ${process.env.PORT}`);
+            })
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+)()
+*/
